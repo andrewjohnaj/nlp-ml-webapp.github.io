f8ab0eba821a795341c662a144c06b1810486c68
andrewjohnaj <reachandrewjohn@gmail.com>
2020-11-24 14:35:22 +0530

    Updates

---
+++ precache-manifest.a188fcdd38726c20106f1038e17af24b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71a24bba16b47c3ad72a",
+    "url": "/andrewjohnaj/nlp-ml-webapp.github.io/static/css/main.11302b67.chunk.css"
+  },
+  {
+    "revision": "71a24bba16b47c3ad72a",
+    "url": "/andrewjohnaj/nlp-ml-webapp.github.io/static/js/main.4cd7854e.chunk.js"
+  },
+  {
+    "revision": "b6d91608be9775c20541",
+    "url": "/andrewjohnaj/nlp-ml-webapp.github.io/static/js/runtime~main.ddf72be7.js"
+  },
+  {
+    "revision": "2cfeae10262681659836",
+    "url": "/andrewjohnaj/nlp-ml-webapp.github.io/static/css/2.05f38cfa.chunk.css"
+  },
+  {
+    "revision": "2cfeae10262681659836",
+    "url": "/andrewjohnaj/nlp-ml-webapp.github.io/static/js/2.b32ff617.chunk.js"
+  },
+  {
+    "revision": "524b341cad075db3363e2ec8dde18f99",
+    "url": "/andrewjohnaj/nlp-ml-webapp.github.io/static/media/web_bg.524b341c.jpg"
+  },
+  {
+    "revision": "4ca8f015f6e65c77bb848301420c14a8",
+    "url": "/andrewjohnaj/nlp-ml-webapp.github.io/index.html"
+  }
+];
